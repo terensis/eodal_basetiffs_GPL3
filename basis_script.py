4d9d00611d7f6b54669c75bca9463a5b8be5e8c3
lukas <lukasvalentin.graf@usys.ethz.ch>
2023-09-28 13:58:11 +0200

    cleaning up

--- basis_script.py
+++ basis_script.py
@@ -8,7 +8,6 @@ GPL 3 license
 25.09.2023
 """
 
-
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from datetime import datetime
@@ -26,14 +25,18 @@ base_dir: Path = Path()
 data_dir: Path = Path(base_dir.joinpath("data"))
 
 # define time range
-time_start: datetime = datetime(2023, 6, 1)  # year, month, day (incl.)
-time_end: datetime = datetime(2023, 9, 26)  # year, month, day (incl.)
+time_start: datetime = datetime(2023, 9, 1)  # year, month, day (incl.)
+time_end: datetime = datetime(2023, 9, 28)  # year, month, day (incl.)
 
 # Spatial Feature
-geom: Path = Path(data_dir.joinpath("Witzwil_BoundingBox_WGS84.geojson"))
+data_dir = Path("data")
+data_dir.mkdir(exist_ok=True)
+# geom: Path = gpd.read_file(data_dir.joinpath("canton_sh.gpkg")).dissolve()
+geom: Path = gpd.read_file(data_dir.joinpath("farm_maranhao.gpkg")).dissolve()
 
 # define project name
-aoi_name = "WITZ"
+# aoi_name = "SH"
+aoi_name = 'FM'
 
 # -----------------------------------------------------------------
 
@@ -44,33 +47,6 @@ out_dir.mkdir(exist_ok=True)
 # Define settings, s.t. we can access the data from MS planetary computer
 Settings = get_settings()
 Settings.USE_STAC = True
-
-
-# TODO: move all inputs not directly needed to a separate file
-# -------------------------- Preprocessing ---------------------------
-def preprocess_sentinel2_scenes(
-    ds: Sentinel2,
-    target_resolution: int,
-) -> Sentinel2:
-    """
-    Resample Sentinel-2 scenes and mask clouds, shadows, and snow
-    based on the Scene Classification Layer (SCL).
-
-    NOTE:
-        Depending on your needs, the pre-processing function can be
-        fully customized using the full power of EOdal and its
-        interfacing libraries!
-
-    :param target_resolution:
-        spatial target resolution to resample all bands to.
-    :returns:
-        resampled, cloud-masked Sentinel-2 scene.
-    """
-    # resample scene
-    ds.resample(inplace=True, target_resolution=target_resolution)
-    # Do not mask clouds
-    # ds.mask_clouds_and_shadows(inplace=True)
-    return ds
 
 
 # -------------------------- Collection -------------------------------
@@ -85,13 +61,13 @@ metadata_filters: List[Filter] = [
 # -------- Define kwargs for the scene constructor ---------------------
 scene_kwargs = {
     "scene_constructor": Sentinel2.from_safe,
-    "scene_constructor_kwargs": {"band_selection": ["B02", "B03", "B04", "B08"]},
-    "scene_modifier": preprocess_sentinel2_scenes,  # our function from above
-    "scene_modifier_kwargs": {"target_resolution": 10},
+    "scene_constructor_kwargs": {
+        "band_selection": ["B02", "B03", "B04", "B08"],
+        'read_scl': False},
 }
 
 # query the scenes available (no I/O of scenes, this only fetches metadata)
-feature = Feature.from_geoseries(gpd.read_file(geom).geometry)
+feature = Feature.from_geoseries(geom.geometry)
 mapper_configs = MapperConfigs(
     collection=collection,
     time_start=time_start,
@@ -128,24 +104,14 @@ for idx, timestamp in enumerate(scene_ts):
     # write out
     out_fname = f"{timestamp.split(' ')[0]}_{aoi_name}_WGS84.tiff"
 
+    # NDVI
+    scene.calc_si('ndvi', inplace=True)
+
     # reproject to wgs84 for web mercator plotting
-    scene_wgs84 = scene.reproject(target_crs=4326)
+    scene_wgs84 = scene.reproject(
+        target_crs=4326)
 
     scene_wgs84.to_rasterio(
-        band_selection=["red", "green", "blue"],
+        band_selection=["red", "green", "blue", "ndvi"],
         fpath_raster=out_dir.joinpath(out_fname),
     )
-
-    """# create naked png and write out
-    fig_naked = scene_wgs84.plot_multiple_bands(
-        ["red", "green", "blue"], figsize=(10, 10)
-    )
-    fig_naked.axes
-    # axs = fig_naked.get_axes
-    plt.axis("off")
-    plt.title(None)
-    plt.tight_layout()
-    fig_naked.savefig(data_dir.joinpath(out_fname + ".png"))
-
-    # save out naked tiff?
-    fig_naked.savefig(data_dir.joinpath("test.tiff"))"""
