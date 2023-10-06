2728194e87ab7ba52c915f4689e776be330a8a62
lukas <lukasvalentin.graf@usys.ethz.ch>
2023-10-06 16:43:22 +0200

    refactoring

--- eodal_viewer/utils.py
+++ eodal_viewer/utils.py
@@ -8,7 +8,73 @@ License: GPLv3
 
 import numpy as np
 
+from datetime import datetime
+from eodal.core.band import Band
 from eodal.core.sensors import Sentinel2
+from pathlib import Path
+
+
+class SceneProcessedException(Exception):
+    pass
+
+
+def get_latest_scene(output_dir) -> datetime:
+    """
+    Get the timestamp of the latest scene from a
+    file called `latest_scene`. If this file does
+    not exist use the default date
+
+    :param output_dir:
+        directory where scenes are stored (in sub-directories)
+    """
+    fpath_latest_scene = output_dir.joinpath('latest_scene')
+    if fpath_latest_scene.exists():
+        with open(output_dir.joinpath('latest_scene'), 'r') as f:
+            timestamp_raw = f.read()
+        timestamp_raw = timestamp_raw.replace('\n', '')
+        timestamp = datetime.strptime(timestamp_raw, '%Y-%m-%d')
+    else:
+        timestamp = datetime(2016, 12, 31)
+    return timestamp
+
+
+def indicate_complete(output_dir_scene: Path) -> None:
+    """
+    Indicate that a scene was extracted and post-
+    processed complete by writing a file named
+    "complete" to the scene sub-directory.
+
+    :param output_dir_scene:
+        output directory of the scene.
+    """
+    fpath_complete = output_dir_scene.joinpath(
+        f'complete.txt'
+    )
+    with open(fpath_complete, 'w') as f:
+        f.write('complete')
+
+
+def make_output_dir_scene(
+    output_dir: Path,
+    timestamp: datetime
+) -> Path:
+    """
+    Make an output directory for a scene. The output directory
+    is name by the sensing date of the scene.
+
+    :param output_dir:
+        directory where to create the scene output sub-directory
+    :param timestamp:
+        sensing time of the scene. Used for the naming of the
+        output directory.
+    """
+    output_dir_scene = output_dir.joinpath(f'{timestamp.date()}')
+    # make sure not to process an existing dataset again
+    if output_dir_scene.exists() and output_dir_scene.joinpath('complete'):
+        raise SceneProcessedException(
+            f'{output_dir_scene} already processed -> skipping')
+    output_dir_scene.mkdir()
+    return output_dir_scene
 
 
 def preprocess_sentinel2_scenes(
@@ -66,7 +132,9 @@ def post_process_scene(
     cloud_mask = np.isin(s2_scene['scl'].values, [3, 8, 9])
     # update cloud mask with the mask of the area of interest,
     # i.e., s2_scene['scl'].values.mask
-    cloud_mask = np.logical_and(cloud_mask, ~s2_scene['scl'].values.mask)
+    if s2_scene['scl'].is_masked_array:
+        cloud_mask = np.logical_and(cloud_mask, ~s2_scene['scl'].values.mask)
+
     # cast to uint8.
     # 0 = no cloud or outside of the area of interest
     # 1 = cloud or cloud shadow
@@ -80,3 +148,20 @@ def post_process_scene(
         geo_info=s2_scene['B02'].geo_info
     )
     return s2_scene
+
+
+def set_latest_scene(
+        output_dir: Path,
+        timestamp: datetime
+) -> None:
+    """
+    Set the timestamp of the latest scene
+    to a file called `latest_scene`.
+
+    :param output_dir:
+        directory where scenes are stored (in sub-directories)
+    :param timestamp:
+        time stamp of the latest scene
+    """
+    with open(output_dir.joinpath('latest_scene'), 'w+') as f:
+        f.write(f'{timestamp.date()}')
