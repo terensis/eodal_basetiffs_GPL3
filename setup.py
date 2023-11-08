afefd4dfc3d590f110f46c3969cec6817d236d50
lukas <lukasvalentin.graf@usys.ethz.ch>
2023-11-08 20:31:40 +0100

    fixed entry point for CLI script

---
+++ setup.py
@@ -0,0 +1,23 @@
+
+from setuptools import setup
+
+
+setup(
+    author='Lukas Valentin Graf, Terensis, Zurich, Switzerland',
+    name='eodal_basetiffs',
+    version=0.1,
+    packages=['eodal_basetiffs'],
+    entry_points={
+        'console_scripts': [
+            'eodal_basetiffs = eodal_basetiffs.main:cli'
+        ]
+    },
+    license='GPLv3',
+    long_description=open('README.md').read(),
+    description='A tool to download satellite data, pre-process it and store it as cloud-optimized GeoTIFFs based on EOdal.',
+    install_requires=[
+        'eodal',
+        'geopandas',
+        'numpy'
+    ],
+)
