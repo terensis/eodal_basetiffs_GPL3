542cbde27147e921a62ff9276ceb7c23613639ed
lukas <graflu@ethz.ch>
2023-12-03 16:17:02 +0100

    increased version to `1.1` and added `UTL`

--- eodal_basetiffs/__meta__.py
+++ eodal_basetiffs/__meta__.py
@@ -8,6 +8,6 @@ author = (
 )
 author_email = "lukas.graf@terensis.io"
 description = "A tool to download satellite data, pre-process it and store it as cloud-optimized GeoTIFFs based on EOdal."
-url = ""
+url = "https://github.com/terensis/eodal_basetiffs_GPL3"
 license = "GNU General Public License version 3"
-version = "0.1"
+version = "1.1"
